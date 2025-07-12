0fb11ac2265bee137735e5539539ac7a94a0eb82
Aditya Domle <adityadomle14@gmail.com>
2025-07-12 16:29:47 +0530

    Added the Card.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import Navbar from "./Components/Navbar";
+import React, { useState } from 'react'
 
-function App(){
-  return <Navbar></Navbar>
+function App() {
+  const [Val, setVal] = useState({ name: "Aditya", isBanned: false })
+
+  return (
+    <div>
+      <h1>name: {Val.name}</h1> {/* 'Name' → should be lowercase 'name' */}
+      <h2>banned: {Val.isBanned.toString()}</h2> {/* Missing () to call toString */}
+      <button onClick={() => setVal({ ...Val, isBanned: !Val.isBanned })} className='px-3 py-11 bg-blue-800 rounded'></button>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="w-full h-screen bg-zinc-300 flex flex-col gap-10 justify-center item-center">
+      <div className="px-3 py-2 bg-zinc-100 rounded">
+        <h3>Hellio</h3>
+      </div>
+    </div>
+  )
+};
