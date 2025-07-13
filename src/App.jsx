fefd0e61d2f1fbf841680c67e15495b002a1b40e
Aditya Domle <adityadomle14@gmail.com>
2025-07-13 22:42:39 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ function App() {
   return (
     <div>
 
-
-      
       <h1>name: {Val.name}</h1> {/* 'Name' → should be lowercase 'name' */}
       <h2>banned: {Val.isBanned.toString()}</h2> {/* Missing () to call toString */}
       <button onClick={() => setVal({ ...Val, isBanned: !Val.isBanned })} className='px-3 py-11 bg-blue-800 rounded'></button>
